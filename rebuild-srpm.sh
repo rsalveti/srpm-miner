6b086d661f452f3a6f97352b0a35ddf9a54806fd
Ricardo Salveti <ricardo.salveti@linaro.org>
2016-10-19 23:38:37 -0200

    rebuild-srpm.sh: build just the source package
    Signed-off-by: Ricardo Salveti <ricardo.salveti@linaro.org>

--- rebuild-srpm.sh
+++ rebuild-srpm.sh
@@ -2,6 +2,6 @@
 yum-builddep -y $1
 rpm -i $1
 pkg=$(basename $1 | sed 's/-[0-9].*//')
-rpmbuild -ba ~/rpmbuild/SPECS/$pkg.spec
+rpmbuild -bs ~/rpmbuild/SPECS/$pkg.spec
 cp ~/rpmbuild/SRPMS/$pkg* $(dirname $1)
 rm $1
